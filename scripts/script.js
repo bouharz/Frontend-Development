4b6e9e741c43676a4cc5829e10acd8c837a89d9c
bouharz <78428629+bouharz@users.noreply.github.com>
2023-10-04 15:51:02 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,26 @@
 // JavaScript Document
-console.log("hi");
+//var btnMenu = document.querySelector("nav i");
+//var menuForm = document.querySelector("nav ul");
+var btnHam = document.querySelector(".hamburger");
+var mobMenu = document.querySelector(".mobile");
+var noScroll = document.querySelector("body");
+var overLay = document.querySelector(".overlay");
+
+//btnMenu.addEventListener("click", openMenu);
+//btnMenu.addEventListener("click", changeBtn);
+btnHam.addEventListener("click", hamOpen);
+
+//function openMenu() {
+//	menuForm.classList.toggle("toonMenu");
+//}
+
+//function changeBtn() {
+//	btnMenu.classList.toggle("toonMenu");
+//}
+
+function hamOpen() {
+	btnHam.classList.toggle("active");
+	mobMenu.classList.toggle("open");
+	noScroll.classList.toggle("scroll");
+	overLay.classList.toggle("on");
+}
